235766b56b5932b95f7435308fdc5df8032eae50
ThyFriendlyFox <thyfriendlyfox@gmail.com>
2025-07-12 05:38:10 -0400

    Main page done, initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+// No script needed for static layout
+</script>
+
+<div class="min-h-screen flex flex-col justify-between bg-black text-white">
+  <div class="flex-1 flex flex-col items-center justify-center">
+    <div class="flex flex-col" style="gap:0;">
+      <div class="text-[6vw] font-normal text-left leading-none" style="font-family: Helvetica, Arial, sans-serif;">Data</div>
+      <div class="text-[6vw] font-normal text-left leading-none" style="font-family: Helvetica, Arial, sans-serif;">Model</div>
+      <div class="text-[6vw] font-normal text-right leading-none" style="font-family: Helvetica, Arial, sans-serif; margin-top:-8px;">Agent</div>
+      <div class="text-[6vw] font-normal text-right leading-none" style="font-family: Helvetica, Arial, sans-serif;">App</div>
+      <div class="text-[6vw] font-normal text-right leading-none" style="font-family: Helvetica, Arial, sans-serif;">Repeat</div>
+    </div>
+  </div>
+  <div class="w-full flex justify-center pb-24">
+    <div class="flex space-x-16">
+      <div class="text-lg" style="font-family: 'Courier New', Courier, monospace;">Pitch</div>
+      <div class="text-lg" style="font-family: 'Courier New', Courier, monospace;">Discord</div>
+      <div class="text-lg" style="font-family: 'Courier New', Courier, monospace;">Waitlist</div>
+      <div class="text-lg" style="font-family: 'Courier New', Courier, monospace;">Social</div>
+      <div class="text-lg" style="font-family: 'Courier New', Courier, monospace;">Team</div>
+    </div>
+  </div>
+</div>
